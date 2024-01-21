5ab2895be3184299cd16c99c32a1039456331201
Matthew <matthewyungisworking@gmail.com>
2024-01-21 01:13:06 -0800

    added google auth basic

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,12 @@
+'use client'
 import React from 'react';
 import styles from '../page.module.css';
+import { signInWithGoogle, signOutUser } from './auth';
+import { onAuthStateChanged } from 'firebase/auth';
+
 export default function Login() {
+
+  
     return (
       <main className={styles.main}>
         <p className={styles.tagline}>Accessible indoor maps for all.</p>
@@ -12,7 +18,7 @@ export default function Login() {
           </div> */}
           <p className={styles.text}>Create an account</p>
           <div className={styles.buttons}>
-            <button className={styles.signin_button} type="submit">
+            <button onClick={signInWithGoogle} className={styles.signin_button} type="submit">
               <img className={styles.google} src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1200px-Google_%22G%22_logo.svg.png" alt="google logo" />
               Sign Up With Google
             </button>
