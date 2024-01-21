61d5b0361440f7edb32b266a521ea4006ac84046
adiarora7 <96757084+adiarora7@users.noreply.github.com>
2024-01-21 06:30:10 -0800

    homepage
    responsive map with search bar

---
+++ src/app/components/BuildingCard/BuildingCard.tsx
@@ -0,0 +1,21 @@
+import { Location } from "../Search/SearchBox"
+import styles from "./buildingCard.module.scss";
+
+export default function BuildingCard({
+    coordinates
+  }:{
+    coordinates?: Location
+  }){
+
+    const showCard = false;
+
+    return(
+        <div className={styles.card}>
+            <h2 className={styles.cardtitle}>{coordinates?.name}</h2>
+            <p className={styles.cardaddress}>{coordinates?.address}</p>
+            <div className={styles.accesibilitytags}></div>
+            <div className={styles.cardimg}></div>
+            <div className={styles.button}></div>
+        </div>
+    )
+}
