e952cc4c0bb7ad695d198770d37537240065fb1e
Indy Sowy <indira.sowy@gmail.com>
2024-01-20 21:41:38 -0800

    clean up login frontend

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import styles from '../page.module.css';
-
 export default function Login() {
     return (
       <main className={styles.main}>
         <p className={styles.tagline}>Accessible indoor maps for all.</p>
         <div className={styles.body}>
-          <div className={styles.logo}>
+        <img className={styles.img} src="https://imgur.com/0W2V0Ni.png"/>
+          {/* <div className={styles.logo}>
               <img className={styles.img} src="https://imgur.com/0W2V0Ni.png"/>
               <p className={styles.logoText}>mapcessibility</p>
-          </div>
+          </div> */}
           <p className={styles.text}>Create an account</p>
-          <button className={styles.button}
-            type="submit"
-          >
-          Sign Up With Google
-          </button>
+          <div className={styles.buttons}>
+            <button className={styles.button} type="submit">
+              <img className={styles.google} src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1200px-Google_%22G%22_logo.svg.png" alt="google logo" />
+              Sign Up With Google
+            </button>
+            <button className={styles.button} type="submit"> 
+              <img className={styles.apple} src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/647px-Apple_logo_black.svg.png" alt="apple logo" />
+              Sign Up With Apple
+            </button>
+          </div>
+          <p className={styles.subtext}>Already have an account? <a href="/login" className={styles.link}>Log in</a></p>
         </div>
     </main>
   );
